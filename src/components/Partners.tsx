0f32c1bdecff3cf7df7523fa3e812fab358bcbcb
zefparis <ben.barere@gmail.com>
2026-02-15 10:44:00 +0100

    feat: Add clickable external links to all partner organization cards with official website URLs

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -3,16 +3,16 @@
 import { useLanguage } from "@/hooks/useLanguage";
 
 const partners = [
-  { name: "Ministère des Affaires Étrangères", logo: "/images/partners/mae.png" },
-  { name: "Ministère du Commerce Extérieur", logo: "/images/partners/mce.png" },
-  { name: "Ministère de l'Entrepreneuriat et PME", logo: "/images/partners/mpme.png" },
-  { name: "Ministère du Plan", logo: "/images/partners/mplan.png" },
-  { name: "CSCEC", logo: "/images/partners/cscec.png" },
-  { name: "Maroc Export", logo: "/images/partners/maroc-export.png" },
-  { name: "Banque Mondiale / World Bank", logo: "/images/partners/banque-mondiale.png" },
-  { name: "Min. Coopération Internationale", logo: "/images/partners/mciirf.png" },
-  { name: "Initiative Center", logo: "/images/partners/initiative-center.png" },
-  { name: "ANAPI", logo: "/images/partners/anapi.png" },
+  { name: "Ministère des Affaires Étrangères", logo: "/images/partners/mae.png", url: "https://diplomatie.gouv.cd/" },
+  { name: "Ministère du Commerce Extérieur", logo: "/images/partners/mce.png", url: "https://commerce.gouv.cd/" },
+  { name: "Ministère de l'Entrepreneuriat et PME", logo: "/images/partners/mpme.png", url: "https://pme.gouv.cd/" },
+  { name: "Ministère du Plan", logo: "/images/partners/mplan.png", url: "https://plan.gouv.cd/" },
+  { name: "CSCEC", logo: "/images/partners/cscec.png", url: "https://www.cscec.com/" },
+  { name: "Maroc Export", logo: "/images/partners/maroc-export.png", url: "https://asmex.org/" },
+  { name: "Banque Mondiale / World Bank", logo: "/images/partners/banque-mondiale.png", url: "https://www.worldbank.org/" },
+  { name: "Min. Coopération Internationale", logo: "/images/partners/mciirf.png", url: "https://diplomatie.gouv.cd/" },
+  { name: "Initiative Center", logo: "/images/partners/initiative-center.png", url: "https://easterncongo.org/" },
+  { name: "ANAPI", logo: "/images/partners/anapi.png", url: "https://anapi.cd/" },
 ];
 
 export default function Partners() {
@@ -36,9 +36,12 @@ export default function Partners() {
 
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
           {partners.map((partner) => (
-            <div
+            <a
               key={partner.name}
-              className="group relative flex flex-col items-center justify-center p-8 rounded-2xl border-[3px] border-gray-300/90 bg-white hover:bg-gradient-to-br hover:from-white hover:to-blue-50/30 hover:shadow-2xl hover:border-[#003DA5]/50 hover:-translate-y-1 transition-all duration-300"
+              href={partner.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group relative flex flex-col items-center justify-center p-8 rounded-2xl border-[3px] border-gray-300/90 bg-white hover:bg-gradient-to-br hover:from-white hover:to-blue-50/30 hover:shadow-2xl hover:border-[#003DA5]/50 hover:-translate-y-1 transition-all duration-300 cursor-pointer"
             >
               {/* Decorative corner accent */}
               <div className="absolute top-0 right-0 w-16 h-16 bg-gradient-to-br from-[#C5A55A]/10 to-transparent rounded-bl-3xl opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
@@ -61,7 +64,7 @@ export default function Partners() {
               
               {/* Bottom accent line */}
               <div className="absolute bottom-0 left-1/2 -translate-x-1/2 h-1 w-0 bg-gradient-to-r from-[#003DA5] via-[#C5A55A] to-[#003DA5] group-hover:w-3/4 transition-all duration-500 rounded-t-full" />
-            </div>
+            </a>
           ))}
         </div>
       </div>
